787c2622b1ab6f41e9c687ae5de9123e88d503a0
Jinuk Baek <kuniz37@gmail.com>
2022-08-18 21:19:01 +0900

    Update release script
    - get the version from $GITHUB_REF

--- make.sh
+++ make.sh
@@ -2,4 +2,6 @@
 
 rm ./NaverSearch.alfredworkflow
 cd workflow
+sed "s/{{VERSION_INFO}}/${GITHUB_REF##*/v}/g" < info.plist > info.plist.bak
+mv info.plist.bak info.plist
 zip -r ../NaverSearch.alfredworkflow .
